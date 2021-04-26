546d10d55a29a826884bc2d663d83593a592bd36
sjcardiff <61339666+cardiffinc@users.noreply.github.com>
2021-04-26 17:58:51 +0530

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,53 @@
+import { AppBar, Button, Grid, IconButton, makeStyles, Paper, Toolbar, Typography } from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu';
+
 import React from 'react'
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    height: 'inherit'
+  },
+  homeGrid:{
+    height: '60vh'
+  },
+  homeGridBody: {
+    height: 'inherit'
+  }
+}));
+
 function Home() {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
+  const classes = useStyles();
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Grid container spacing={3} className={classes.homeGrid}>
+        <Grid item xs={6} className={classes.homeGridBody}>
+          <Paper className={classes.paper}>xs=6</Paper>
+        </Grid>
+        <Grid item xs={6} className={classes.homeGridBody}>
+          <Paper className={classes.paper}>xs=6</Paper>
+        </Grid>
+      </Grid>
+    </div>
+  )
 }
 
 export default Home
