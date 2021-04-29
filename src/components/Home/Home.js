13cb520ece2e0283e1347992a4530f05b4d4de32
sjcardiff <61339666+cardiffinc@users.noreply.github.com>
2021-04-29 21:41:14 +0530

    Template Left Grid Styling

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,7 +2,9 @@ import { AppBar, Button, Grid, IconButton, makeStyles, Paper, Toolbar, Typograph
 import MenuIcon from '@material-ui/icons/Menu';
 
 import React from 'react'
+import Lottie from 'react-lottie';
 import SingleLineGrid from '../SingleLineGrid/SingleLineGrid';
+import CovidAnimation from '../../lottie/CovidAnimations.json'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -18,6 +20,22 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
   },
+  templateHeading:{
+    textTransform: 'uppercase',
+    fontSize: 60,
+    marginTop: '2.5em!important',
+    marginBottom: '0em!important'
+  },
+  templateSubTitle: {
+    fontSize: 35,
+    fontWeight: 'lighter',
+    marginTop: '0em!important',
+    marginBottom: '0em!important',
+  },
+  templateParagraph:{
+    marginBottom: '3em!important',
+    fontSize: 12
+  },
   paper: {
     padding: theme.spacing(2),
     textAlign: 'left',
@@ -29,7 +47,12 @@ const useStyles = makeStyles((theme) => ({
     height: '60vh'
   },
   homeGridBody: {
-    height: 'inherit'
+    height: 'inherit',
+    textAlign: 'start',
+    color: '#fff',
+  },
+  homeGridContent:{
+    marginLeft: 20
   },
   mainButton:{
     background: '#fff',
@@ -51,7 +74,15 @@ const useStyles = makeStyles((theme) => ({
 
 function Home() {
   const classes = useStyles();
-  const twelve = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 ]
+  const defaultLottieConfig = {
+    loop: true,
+    autoplay: true,
+    animationData: CovidAnimation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  };
+
   return (
     <div>
       <AppBar position="static" className={classes.appBar}>
@@ -63,14 +94,20 @@ function Home() {
       </AppBar>
       <Grid container spacing={3} className={classes.homeGrid}>
         <Grid item xs={6} className={classes.homeGridBody}>
-            <h1>Corona</h1>
-            <h2>Virus Disease (COVID-19)</h2>
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eu volutpat quam. Nunc vitae mollis libero. Sed vitae blandit massa. Suspendisse fermentum libero non neque viverra consectetur. Fusce justo tellus, ullamcorper et hendrerit in, semper et quam. Sed commodo arcu a viverra volutpat. Maecenas ultrices semper sollicitudin. Maecenas tempus fermentum quam dictum tempor. Sed blandit orci imperdiet mauris euismod, ac vulputate nulla efficitur. Vestibulum accumsan nisi ipsum, id pretium erat hendrerit vitae. Donec a augue erat. Proin orci nunc, vestibulum eu justo vel, accumsan aliquam felis.
-            </p>
-            <Button className={classes.mainButton}>Read More</Button>
+            <div className={classes.homeGridContent}>
+              <h1 className={classes.templateHeading}>Corona</h1>
+              <h2 className={classes.templateSubTitle}>Virus Disease (COVID-19)</h2>
+              <p className={classes.templateParagraph}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eu volutpat quam. Nunc vitae mollis libero. Sed vitae blandit massa. Suspendisse fermentum libero non neque viverra consectetur. Fusce justo tellus, ullamcorper et hendrerit in, semper et quam. Sed commodo arcu a viverra volutpat. Maecenas ultrices semper sollicitudin. Maecenas tempus fermentum quam dictum tempor. Sed blandit orci imperdiet mauris euismod, ac vulputate nulla efficitur. Vestibulum accumsan nisi ipsum, id pretium erat hendrerit vitae. Donec a augue erat. Proin orci nunc, vestibulum eu justo vel, accumsan aliquam felis.
+              </p>
+              <Button className={classes.mainButton}>Read More</Button>
+            </div>
         </Grid>
         <Grid item xs={6} className={classes.homeGridBody}>
+          <Lottie 
+            options={defaultLottieConfig}
+            height={400}
+            width={600}/>
         </Grid>
       </Grid>
       <Grid>
